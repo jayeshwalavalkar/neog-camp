4f8de920521ce41573d84c1ade66db91a98e6d12
jayeshwalavalkar <jayeshwalavalkar7@gmail.com>
2022-02-21 15:20:12 +0000

    quiz completed

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
-var groceryList = ['rice', 'oil'];
+var readlineSync = require('readline-sync');
 
-for (var i=0; i< groceryList.length; i++){
-  console.log('grocery : ', groceryList[i]);
+var score = 0;
+
+var questionOne = {
+  question: 'Who won the fifa world cup in 2014 : ',
+  answer: 'germany'
+};
+
+var questionTwo = {
+  question: 'Who won the fifa world cup in 2010 : ',
+  answer: 'spain'
+};
+
+var questions = [questionOne, questionTwo];
+
+for (var i=0; i<questions.length; i++){
+  var answer = readlineSync.question(questions[i].question);
+  if(answer === questions[i].answer) {
+    score = score + 1;
+  } else {
+    score = score - 1;
+  }
+
 }
+
+console.log('Your final score : ', score);
